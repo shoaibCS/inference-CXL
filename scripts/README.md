12b196d81ba44de88da03a2746a13dfc46492c83
shoaibCS <shoaibCS@node.shoaibcs-191115.nestfarm-pg0.wisc.cloudlab.us>
2024-02-21 04:08:18 -0700

    Added bert automated script

--- scripts/README.md
+++ scripts/README.md
@@ -29,3 +29,11 @@ To run medical imaging benchmark, perform following steps
 - Install libraries with this command 'bash medical.sh 1'
 - Run benchmark with this command 'bash medical.sh 2'
 
+
+## Bert
+
+To run Bert benchmark, perform following steps
+
+- Install libraries with this command 'bash bert.sh 1'
+- Run benchmark with this command 'bash bert.sh 2'
+
--- scripts/bert.sh
+++ scripts/bert.sh
@@ -10,14 +10,25 @@ export TMPDIR=/users/shoaibCS/mnt/tmpd
 export MKL_INTERFACE_LAYER="LP64,GNU"
 if [ "$1" -eq 1 ]; then
 
+conda create -n bert python=3.8 -y
+
 set +u
 source "$(conda info --base)/etc/profile.d/conda.sh"
-conda activate ml
+conda activate bert
 set -u
 whereis python
 
-conda install nibabel -y
-conda install scipy -y
+pip3 install pybind11
+conda install tensorflow -y
+conda install numpy -y
+conda install transformers -y
+
+cd ../loadgen
+CFLAGS="-std=c++14 -O3" python setup.py bdist_wheel
+pip3 install --force-reinstall dist/mlperf_loadgen-4.0-cp38-cp38-linux_x86_64.whl
+
+whereis python
+
 
 cd ../language/bert/
 
@@ -29,59 +40,13 @@ elif [ "$1" -eq 2 ]; then
 
 #conda create -n bert python=3.9 -y
 
-conda env create --force -v --file bert.yml
 
 set +u
 source "$(conda info --base)/etc/profile.d/conda.sh"
-conda activate berta
+conda activate bert
 set -u
 
-pip3 install --force-reinstall numpy
-# install pytorch
-# you can find other nightly version in https://download.pytorch.org/whl/nightly/
 
-
-#pip3 install https://download.pytorch.org/whl/nightly/cpu-cxx11-abi/torch-2.0.0.dev20230228%2Bcpu.cxx11.abi-cp39-cp39-linux_x86_64.whl
-
-#rust pyarrow
-
-echo "2"
-sudo apt-get install python3-pip -y
-# installation
-pip3 install setuptools_rust
-sudo apt-get install build-essential -y
-pip3 install Cython --user
-conda install pyarrow -y
-#pip3 install pyarrow>=6.0.0
-#sudo apt-get install build-essential
-
-pip3 install transformers datasets
-pip3 install evaluate accelerate simplejson nltk rouge_score
-#pip3 evaluate
-#pip3 install setuptools_rust
-#pip3 accelerate 
-#pip3 simplejson 
-
-#pip3 nltk 
-#pip3 rouge_score
-
-pip3 install pybind11
-cd ../loadgen
-CFLAGS="-std=c++14 -O3" python setup.py bdist_wheel
-
-
-cd ..; pip3 install --force-reinstall loadgen/dist/`ls -r loadgen/dist/ | head -n1` ; cd -
-ls
-pwd
-echo "-----------------------------------------------------------------------------------------------------"
-
-
-
-cd ../scripts
-
-
-
-set -u
 cp ../data/bert_mlperf.conf ../language/bert/build/mlperf.conf
 
 cd ../language/bert && $CONDA_PREFIX/bin/python3 run.py --backend=tf --scenario=Offline --max_examples=1
